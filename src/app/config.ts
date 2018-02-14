bb8c4aee2390041681f96a90c7f451166cadeaf8
Michalis Karasavvas <karasavm@gmail.com>
2018-02-14 23:01:09 +0200

    api host chagnge

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,7 @@
 // export const host = 'http://localhost:3000/api';
-export const host = 'http://192.168.1.5:3000/api';
+// export const host = 'http://192.168.1.5:3000/api';
 // export const host = 'http://10.0.0.153:3000/api';
-// export const host = 'http://138.68.180.1/api';
+export const host = 'http://138.68.180.1/api';
 // export const host = 'http://c8d030d1.ngrok.io/api';
 
 
